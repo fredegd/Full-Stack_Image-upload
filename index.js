d89f5fdf7736329a19c754bd9f2d0b7303f3a283
fredegd <124848643+fredegd@users.noreply.github.com>
2023-07-03 10:29:11 +0200

    setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv/config");
+require("./db");
+const cors = require("cors");
+//
+//const userRouter = require("./routes/users");
+//const propertyRouter = require("./routes/properties");
+//
+const app = express();
+const PORT = process.env.PORT || 3000;
+/////////////
+//MIDDLEWARES
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use(express.json());
+/////////
+//ROUTES
+//app.use("/users", userRouter);
+//app.use("/properties", propertyRouter);
+
+app.listen(PORT, () => {
+  console.log(`Real Estate App is Listening on http://localhost:${PORT}`);
+});
